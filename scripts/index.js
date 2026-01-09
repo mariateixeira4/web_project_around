31dcccb850fcd9aec0222f4c6f5eb046a65c4e3e
Maria Eduarda <correia.mariaeduarda123@gmail.com>
2026-01-09 15:40:12 +0000

    Nova atualização do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,11 @@ const addButton = document.querySelector(".profile__add-button");
 
 const popupProfile = document.querySelector("#editProfilePopup");
 const popupNewLocal = document.querySelector("#addPostPopup");
+const popupImage = document.querySelector("#imagePopup");
 
 const closeButtonEdit = popupProfile.querySelector(".popup__close-button");
 const closeButtonAdd = popupNewLocal.querySelector(".popup__close-button");
+const closeButtonImage = popupImage.querySelector(".popup__close-button");
 
 const popupProfileForm = popupProfile.querySelector(".popup__form");
 const popupNewLocalForm = popupNewLocal.querySelector(".popup__form");
@@ -20,6 +22,9 @@ const profileName = document.querySelector(".profile__name");
 const profileText = document.querySelector(".profile__text");
 
 const cardsWrapper = document.querySelector(".elements__list");
+
+const popupImageElement = popupImage.querySelector(".popup__image");
+const popupImageCaption = popupImage.querySelector(".popup__caption");
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -39,6 +44,10 @@ closeButtonEdit.addEventListener("click", () => {
   closePopup(popupProfile);
 });
 
+closeButtonImage.addEventListener("click", () => {
+  closePopup(popupImage);
+});
+
 popupProfileForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -47,12 +56,30 @@ popupProfileForm.addEventListener("submit", (evt) => {
 });
 
 const initialCards = [
-  { name: "Vale de Yosemite", link: "images/Vale.png" },
-  { name: "Lago Louise", link: "images/Louise.png" },
-  { name: "Montanhas Carecas", link: "images/Montanha.png" },
-  { name: "Latemar", link: "images/Latemar.png" },
-  { name: "Parque Nacional da Vanoise", link: "images/Parque.png" },
-  { name: "Lago di Braies", link: "images/Lago.png" },
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
 ];
 
 const cardTemplate = document.querySelector("#card-template");
@@ -63,6 +90,7 @@ function renderCard(cardData) {
   const image = cardElement.querySelector(".element__image");
   const title = cardElement.querySelector(".element__title");
   const likeButton = cardElement.querySelector(".element__like-button");
+  const deleteButton = cardElement.querySelector(".element__delete-button");
 
   image.src = cardData.link;
   image.alt = cardData.name;
@@ -70,6 +98,18 @@ function renderCard(cardData) {
 
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("element__like-button_active");
+  });
+
+  deleteButton.addEventListener("click", (evt) => {
+    const card = evt.target.closest(".element");
+    card.remove();
+  });
+
+  image.addEventListener("click", () => {
+    popupImageElement.src = cardData.link;
+    popupImageElement.alt = cardData.name;
+    popupImageCaption.textContent = cardData.name;
+    openPopup(popupImage);
   });
 
   cardsWrapper.append(cardElement);
